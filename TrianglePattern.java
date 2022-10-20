fa8b4010185d383321c0a3afd7b2f5fc37c619cd
jobin jose <jobinjose.in@gmail.com>
2022-10-20 20:06:39 +0530

    Generate a triangle using numbers

--- TrianglePattern.java
+++ TrianglePattern.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class trianglePattern {
+public class TrianglePattern {
     public static void main(String a[]) {
         Scanner sc = new Scanner(System.in);
 
