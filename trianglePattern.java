75c1be048c2faf1f02584dcaed44466061f28973
jobin jose <jobinjose.in@gmail.com>
2022-10-20 13:03:31 +0530

    Generate a triangle using number patterns

---
+++ trianglePattern.java
@@ -0,0 +1,21 @@
+// Java program to generate a triangle.
+
+import java.util.Scanner;
+
+public class trianglePattern {
+    public static void main(String a[]) {
+        Scanner sc = new Scanner(System.in);
+
+        //Read user input.
+        System.out.print("Enter the limit value:");
+        int limitValue = sc.nextInt();
+
+        //Nested loop to print the corresponding triangle.
+        for(int i=0; i<=limitValue; i++) {
+            for(int j=0; j<i; j++) {
+                System.out.print(String.format("%d\t", i));
+            }
+            System.out.print("\n");
+        }
+    }
+}
